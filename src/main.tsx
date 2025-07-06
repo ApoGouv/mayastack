37e4d62b13ab9c7744cb46f1f9726500659dba5c
ApostolosG <apo.gouv@gmail.com>
2025-07-06 20:23:42 +0300

    feat: add React Router and page layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx'
+import HomeView from './views/HomeView.tsx';
+import LearnView from './views/LearnView.tsx';
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomeView />} />
+          <Route path="learn" element={<LearnView />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
